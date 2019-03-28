79eab79194e451d5629ec97f6e549682b866bf7a
ZYV037 <461177348@qq.com>
2019-03-28 17:35:59 +0800

    Singleton

---
+++ Singleton_example.cpp
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+/*  Best Singleton
+    noncopyable
+    use stack, not new from heap
+    easy use
+*/
+
+#define  Declare_Singleton(classname) \
+public: \
+static classname& Instance() \
+{ \
+    static classname _instance; \
+    return _instance; \
+} \
+protected: \
+    classname() = default; \
+    ~classname() = default; \
+                            \
+    classname(const classname& rInstance); \
+    classname& operator = (const classname& rInstance) \
+
+
+class XmlConfigManager
+{
+    Declare_Singleton(XmlConfigManager);
+
+public:
+    bool add(int a, int b){ return a+b;};
+};
+
+#define XmlManager (XmlConfigManager::Instance())
+
+#include <iostream>
+
+int main()
+{
+    std::cout << XmlManager.add(1, 2) << std::endl;
+    return 0;
+}
